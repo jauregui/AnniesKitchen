eed2ae63f7162e1846b89d38db9f1dfb60e9c840
David Quilla <davichoq@gmail.com>
2016-04-29 08:49:57 -0500

    Contacto

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -3,14 +3,7 @@
 
   <div class="contact-us">
 
-    <div class="contact-info .col-md-8" style="list-style-type:none;
-  background-color: #4c4c4c;
-    position: fixed;
-    left: 30px;
-    top: 100px;
-    padding: 20px;
-    border: solid;
-    font-size:16px">
+    <div class="contact-info .col-md-8">
         <ul>
           <li>Direccion: Distrito B-6 410,Cumbres 1º Sector, 64610, Monterrey, N.L.</li>
           <li>Telefono:8182805191</li>
@@ -20,15 +13,7 @@
     </div>
 
     <div class="contact-img .col-md-4">
-       <img href="/login" src="../images/logo3.png" alt="logo" height="300" width="200" id="img-logo" style="position:fixed;
-  float:right;
-  right: 100px;
-  top:30px;
-  margin-right: 20px;
-  display: inline;
-  padding: 10px;
-  background-color: #9AEEAA;
-  border:solid 2px white;">
+       <img href="/login" src="../images/logo3.png" alt="logo" height="50" width="50">
     </div>
 
   </div>
