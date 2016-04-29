9b62457dc615c64f7d9e0e3bb7623567f6684a3b
David Quilla <davichoq@gmail.com>
2016-04-29 08:44:41 -0500

    Nada

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -5,10 +5,10 @@
 
     <div class="contact-info .col-md-8">
         <ul>
-          <li>Direccio&oacute;n: Distrito B-6 410,Cumbres 1º Sector, 64610, Monterrey, N.L.</li>
-          <li>Tel&eacute;fono: 8182805191</li>
-          <li>Correo Electr&oacute;nico: anniekitchen@gmail.com</li>
-          <li>Horario de atenci&oacute;n: 9:00 am - 6:00 pm</li>
+          <li>Direccion: Distrito B-6 410,Cumbres 1º Sector, 64610, Monterrey, N.L.</li>
+          <li>Telefono:8182805191</li>
+          <li>Correo Electronico:anniekitchen@gmail.com</li>
+          <li>Horario de atencion: 9:00am - 6:00pm</li>
         </ul>
     </div>
 
