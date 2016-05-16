7ef90494d57870425d5e65bcd02112b1b4911860
David Valles <davs93@hotmail.com>
2016-05-16 18:08:04 -0500

    contacto

--- resources/views/Nosotros.blade.php
+++ resources/views/Nosotros.blade.php
@@ -1,11 +1,22 @@
-@extends('layouts.inicio')
+@extends('layouts.newApp')
 @section('content')
-<img id="logo" src="../images/logo4.png"></img>
-<img id="muestra" src="../images/platilloMuestra.png"></img>
 
-<p id="texto1">Lorem ipsum ad his scripta blandit partiendo, eum fastidii accumsan euripidis in, eum liber hendrerit an. Qui ut wisi vocibus suscipiantur, quo dicit ridens inciderint id. Quo mundi lobortis reformidans eu, legimus senserit definiebas an eos. Eu sit tincidunt incorrupte definitionem, vis mutat affert percipit cu, eirmod consectetuer signiferumque eu per. In usu latine equidem dolores. Quo no falli viris intellegam, ut fugit veritus placerat per.
-Ius id vidit volumus mandamus, vide veritus democritum te nec, ei eos debet libris consulatu. No mei ferri graeco dicunt, ad cum veri accommodare. Sed at malis omnesque delicata, usu et iusto zzril meliore. Dicunt maiorum eloquentiam cum cu, sit summo dolor essent te. Ne quodsi nusquam legendos has, ea dicit voluptua eloquentiam pro, ad sit quas qualisque. Eos vocibus deserunt quaestio ei..</p>
-<p id="texto2">Sed iusto nihil populo an, ex pro novum homero cotidieque. Te utamur civibus eleifend qui, nam ei brute doming concludaturque, modo aliquam facilisi nec no. Vidisse maiestatis constituam eu his, esse pertinacia intellegam ius cu. Eos ei odio veniam, eu sumo altera adipisci eam, mea audiam prodesset persequeris ea. Ad vitae dictas vituperata sed, eum posse labore postulant id. Te eligendi principes dignissim sit, te vel dicant officiis repudiandae.
-Id vel sensibus honestatis omittantur, vel cu nobis commune patrioque. In accusata definiebas qui, id tale malorum dolorem sed, solum clita phaedrum ne his. Eos mutat ullum forensibus ex, wisi perfecto urbanitas cu eam, no vis dicunt impetus. Assum novum in pri, vix an suavitate moderatius, id has reformidans referrentur. Elit inciderint omittantur duo ut, dicit democritum signiferumque eu est, ad suscipit delectus mandamus duo. An harum equidem maiestatis nec.</p>
-<img id="cocinera" src="../images/cocinera.png"></img>
+<div class="nosotros">
+
+	<div class="intro">
+		<div align="center" class="i-text">Nosotros</div>
+	</div>
+</div>
+
+<div class="even col-sm-12">
+	<h1 class ="parraf-titulo">Misión</h1>
+	<div class = "parraf-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+</div>
+
+<div class="odd col-sm-12">
+	<h1 class ="parraf-titulo">Visión</h1>
+	<div class = "parraf-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+</div>
+
+
 @endsection
--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -1,29 +1,50 @@
-@extends('layouts.inicio')
+@extends('layouts.newApp')
 @section('content')
 
-  <div class="contact-us">
+<div class="contacto">
 
-    <div class="contact-info .col-md-8">
+  <div class="intro">
+    <div align="center" class="i-text">Contacto</div>
+  </div>
+</div>
+
+<div class="odd col-sm-12">
+  <h1 class ="parraf-titulo">Escríbenos tus mensajes, sugerencias o peticiones</h1>
+  <div class = "parraf-text">
+   <form class="form_contact" action="contact.php" method="post">
+
+         <label for="text" class="col-sm-2 control-label">Nombre</label>
+         <div class="col-sm-10">
+          <input class="form-control "placeholder="Tu Nombre" type="text" name="cf_name" cols="50">
+         </div><br><br>
+
+        <label for="email" class="col-sm-2 control-label">Correo</label>
+         <div class="col-sm-10">
+          <input class="form-control "placeholder="Tu correo" type="text" name="cf_email" cols="50">
+         </div><br><br>
+
+         <label for="text" class="col-sm-2 control-label">Mensaje</label>
+         <div class="col-sm-10">
+          <textarea class="form-control" placeholder="Tu Mensaje" name="cf_message"></textarea><br><br>
+        </div>
+
+        <input type="submit" value="Enviar">
+        <input type="reset" value="Reset">
+      </form>     
+  </div>
+</div>
+
+<div class="even col-sm-12">
+  <h1 class ="parraf-titulo">Datos</h1>
+  <div class = "parraf-text">
         <ul>
           <li>Direccion: Distrito B-6 410,Cumbres 1º Sector, 64610, Monterrey, N.L.</li>
           <li>Telefono:8182805191</li>
           <li>Correo Electronico:anniekitchen@gmail.com</li>
           <li>Horario de atencion: 9:00am - 6:00pm</li>
         </ul>
-        <form action="contact.php" method="post">
-      <h3> Escríbenos tus mensajes, sugerencias o peticiones a "anniekitchen@gmail.com" o utiliza nuestra forma de contacto </h3>
-      Tu nombre<br>
-      <input type="text" name="cf_name"><br>
-      Tu correo<br>
-      <input type="text" name="cf_email"><br>
-      Mensaje<br>
-      <textarea name="cf_message"></textarea>
-      <input type="submit" value="Enviar">
-      <input type="reset" value="Reset">
-      </form>
-    </div>
   </div>
+</div>
 
-  
 
 @endsection
